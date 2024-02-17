18ad4361e3d3a2aee9517a40e8c57970811207b6
Matthew Oellerich <matthew.oellerich@kellogg.northwestern.edu>
2024-02-17 21:12:55 +0000

    progress 2

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,13 +9,44 @@ class PlacesController < ApplicationController
   end
 
   def new
+    @place = Place.new
   end
 
   def create
-    @place = Place.new
-    @place["name"] = params["name"]
+    @place = Place.new(params["place"])
     @place.save
     redirect_to "/places"
+  end
+
+  def edit
+    @place = Place.find_by({ "id" => params["id"] })
+  end
+
+  def update
+    # find a Company
+    @company = Company.find_by({ "id" => params["id"] })
+
+    # assign user-entered form data to Company's columns
+    @company["name"] = params["name"]
+    @company["city"] = params["city"]
+    @company["state"] = params["state"]
+
+    # save Company row
+    @company.save
+
+    # redirect user
+    redirect_to "/companies"
+  end
+
+  def destroy
+    # find a Company
+    @company = Company.find_by({ "id" => params["id"] })
+
+    # destroy Company row
+    @company.destroy
+
+    # redirect user
+    redirect_to "/companies"
   end
 
 end
